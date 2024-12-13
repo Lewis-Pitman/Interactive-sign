d7eafb3be563c0b63fbe3dbae9efa1a141dc6b35
Lewis-Pitman <lpitman08@outlook.com>
2024-12-13 18:36:28 +0000

    Help screen
    Help screen implemented

--- Pages/HelpScreen.xaml.cs
+++ Pages/HelpScreen.xaml.cs
@@ -2,10 +2,65 @@ namespace Interactive_sign;
 
 public partial class HelpScreen : ContentPage
 {
+    private int screen = 1;
+    private string text1 = "On the search tab, you can scroll through categories by swiping your finger up and down on the screen. To see items inside a category, tap on the category.";
+    private string text2 = "When you have tapped on a category, you can swipe your finger up and down to scroll through the available items, and tap on one that you'd like more information on.";
+    private string text3 = "On the events tab, swipe your finger left and right on the timeline to view events. When you see one that interests you, tap on it for more information.";
     public HelpScreen()
     {
         InitializeComponent();
+        UpdateScreen();
     }
+
+    private void UpdateScreen()
+    {
+        switch (screen)
+        {
+            case 2:
+                var image2 = new Image
+                {
+                    Source = "help2.png",
+                    Aspect = Aspect.AspectFill
+                };
+                imageFrame.Content = image2;
+                textLabel.Text = text2;
+                break;
+            case 3:
+                var image3 = new Image
+                {
+                    Source = "help3.png",
+                    Aspect = Aspect.AspectFill
+                };
+                imageFrame.Content = image3;
+                textLabel.Text = text3;
+                break;
+            default:
+                var image1 = new Image
+                {
+                    Source = "help1.png",
+                    Aspect = Aspect.AspectFill
+                };
+                imageFrame.Content = image1;
+                textLabel.Text = text1;
+                break;
+        }
+    }
+
+    // Left/right --------------------------------------------------------------------------------------------------------
+
+    private void LeftClicked(object sender, EventArgs e)
+    {
+        screen = screen <= 1 ? 1 : screen - 1;
+        UpdateScreen();
+    }
+
+    private void RightClicked(object sender, EventArgs e)
+    {
+        screen = screen >= 3 ? 3 : screen + 1;
+        UpdateScreen();
+    }
+
+    //--------------------------------------------------------------------------------------------------------------------
 
     // Cancel button -----------------------------------------------------------------------------------------------------
     private void CancelPressed(object sender, EventArgs e) { cancelButton.BackgroundColor = Color.FromArgb("863838"); }
